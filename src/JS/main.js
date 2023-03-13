c5636276f57805a9e3834e7ddff96756c4af0c29
pranjal2403 <pranjalsingh@cedcoss.com>
2023-03-13 13:02:55 +0530

    Constructing Strings with Variables

--- src/JS/main.js
+++ src/JS/main.js
@@ -1,4 +1,4 @@
-//Decrement a Number with JavaScript
+/*Decrement a Number with JavaScript
  let myVar = 11;
 
 // Only change code below this line
@@ -12,3 +12,63 @@ const quotient = 4.4 / 2.0; // Change this line
 
 //Finding a Remainder in JavaScript
 const remainder = 11%3;
+
+//Compound Assignment With Augmented Addition
+let a = 3;
+let b = 17;
+let c = 12;
+
+// Only change code below this line
+a+= 12;
+b+= 9;
+c+= 7;
+
+//Compound Assignment With Augmented Subtraction
+let a = 11;
+let b = 9;
+let c = 3;
+
+// Only change code below this line
+a-= 6;
+b-= 15;
+c-= 1;
+
+//Compound Assignment With Augmented Multiplication
+let a = 5;
+let b = 12;
+let c = 4.6;
+
+// Only change code below this line
+a*= 5;
+b*= 3;
+c*= 10;
+
+//Compound Assignment With Augmented Division
+let a = 48;
+let b = 108;
+let c = 33;
+
+// Only change code below this line
+a/= 12;
+b/= 4;
+c/= 11*/
+
+/*//Escaping Literal Quotes in Strings
+const myStr = "I am a \"double quoted\" string inside \"double quotes\"."; // Change this line
+
+//Quoting Strings with Single Quotes
+const myStr ='<a href="http://www.example.com" target="_blank">Link</a>';
+
+//Escape Sequences in Strings
+const myStr = "FirstLine\n\t\\SecondLine\nThirdLine";
+
+//Concatenating Strings with Plus Operator
+const myStr = "This is the start."+" "+ "This is the end.";
+
+//Concatenating Strings with the Plus Equals Operator
+let myStr = "This is the first sentence.";
+myStr += " "+"This is the second sentence.";*/
+
+//Constructing Strings with Variables
+const myName = "pranjal singh";
+const myStr = "My name is " + myName + "and I am well!";
