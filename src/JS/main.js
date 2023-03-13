6b6fea917ec25d14437cdccaf48209ae7c246fee
pranjal2403 <pranjalsingh@cedcoss.com>
2023-03-13 12:26:24 +0530

    Finding a Remainder in JavaScript

--- src/JS/main.js
+++ src/JS/main.js
@@ -3,3 +3,12 @@
 
 // Only change code below this line
 myVar = myVar - 1;
+
+//Multiply Two Decimals with JavaScript
+const product = 2.0 * 2.5;
+
+//Divide One Decimal by Another with JavaScript
+const quotient = 4.4 / 2.0; // Change this line
+
+//Finding a Remainder in JavaScript
+const remainder = 11%3;
